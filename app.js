42645e93a0680e009aac9ce1153d14cf003b104d
Paul Betts <paul@paulbetts.org>
2015-07-01 00:46:06 -0700

    Create a BrowserWindow to open our app

---
+++ app.js
@@ -0,0 +1,21 @@
+var app = require('app')
+var BrowserWindow = require('browser-window')
+var path = require('path')
+
+var win
+
+app.on('ready', function () {
+  require('electron-compile').init();
+
+  win = new BrowserWindow({
+    title: 'Polymer Starter Kit, Electron Edition',
+    width: 860,
+    height: 470,
+    show: false
+  })
+
+  win.loadUrl('file://' + path.join(__dirname, 'app/index.html#' + JSON.stringify(process.argv.slice(2))))
+  win.show()
+
+  setTimeout(function() { win.openDevTools() }, 2000);
+})
